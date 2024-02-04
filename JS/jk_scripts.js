0f0a44b8357a03f5e55d4f5436b5422733362e37
AnalysisForBananas <58336880+KvasnickaJ@users.noreply.github.com>
2024-02-04 12:20:27 +0100

    Addend change log
    # Changelog
    
    All notable changes to this website will be documented in this page.
    The format is based on [Keep a Changelog]
    
    ## [Unreleased]
    
    
    ## 2024-02-02
    
    ### Added
    - Google Analytic integration for traffic monitoring to support decision how much time dedicate to this project.
    - Consent form to choice use of google analytics
    - Privacy policy to be comlaient with GDPR.
    
    
    ## 2024-01-31
    
    ### Added
    - Changelog website
    - Picture placeholders for About Me website replaced with pictures
    
    ### Changed
    - Picture placeholders for About Me website replaced with pictures
    
    
    ## 2023-07-02
    
    ### Added
    - Index page
    - About me page
    - Personal project page
    - Portfolio page

---
+++ JS/jk_scripts.js
@@ -0,0 +1,11 @@
+		 // To fade away the page before loading new one 
+         window.transitionToPage = function(href) {
+             document.querySelector('body').style.opacity = 0
+             setTimeout(function() { 
+                 window.location.href = href
+             }, 500)
+         };
+         
+         document.addEventListener('DOMContentLoaded', function(event) {
+             document.querySelector('body').style.opacity = 1
+         });
